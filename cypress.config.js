4e8ce9b255bee19843faf34c84858d513888c3dd
Kamil Knapik <knapik.kamil@outlook.com>
2025-02-23 18:31:43 +0100

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://www.automationpractice.pl/index.php",
+    redirectionLimit: 3,
+    retries: {
+      runMode: 1,
+      openMode: 1,
+    },
+    watchForFileChanges: true,
+    chromeWebSecurity: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    waitForAnimations: true,
+    testIsolation: false,
   },
 });
